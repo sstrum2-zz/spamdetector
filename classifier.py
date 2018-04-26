952356d185036abddead0f96e5dc4dcddc3c7839
sstrum2 <sstrum2@illinois.edu>
2018-04-26 17:22:02 -0500

    adding classifier and sample dataset

---
+++ classifier.py
@@ -0,0 +1,39 @@
+#Classifies email snippets as spam or non-spam, using a Naive Bayes' Classifier
+#Spam is defined as any snippet where P(Spam) > P(Non-Spam)
+import pandas as pd
+
+def classifier():
+    print("in classifier")
+
+def trainer():
+    trainingSet = pd.read_csv("dataset.csv")
+    print(trainingSet)
+    msgCount = 0
+    numSpam = 0
+    numHam = 0
+
+    print(trainingSet['v1'])
+
+    for row in trainingSet.iterrows():
+        answer = row[1]['v1']
+        msg = row[1]['v2']
+
+        if (answer == "spam"):
+            numSpam += 1
+        else:
+            numHam += 1
+        msgCount += 1
+
+    print("NumSpam: " + str(numSpam))
+    print("NumHam: " + str(numHam))
+
+
+def tester():
+    myVar = 0
+    #print("hi")
+
+
+if __name__ == "__main__":
+    classifier()
+    trainer()
+    tester()
